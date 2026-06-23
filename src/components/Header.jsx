6623f4207b4c499a044a80a1e7b634c11f010b8d
M. Nadhir Fauzananto <nadhir.fauzananto@gmail.com>
2026-06-23 15:11:58 +0200

    update header
    make language-selector also usable with onclick

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 const githubIcon = import.meta.env.BASE_URL + 'images/icon-github.svg'
 const frontendMentorIcon = import.meta.env.BASE_URL + 'images/icon-frontend-mentor.svg'
@@ -8,6 +8,7 @@ const germanFlag = import.meta.env.BASE_URL + 'images/german-flag.svg'
 const indonesiaFlag = import.meta.env.BASE_URL + 'images/indonesia-flag.svg'
 
 export default function Header() {
+    const [isOpen, setIsOpen] = useState(false)
     const {i18n} = useTranslation()
     return (
         <header>
@@ -26,21 +27,32 @@ export default function Header() {
                     <img className="frontend-mentor-icon icon" src={frontendMentorIcon} alt="to frontend mentor profile"></img>
                 </a>
                 <div className="language-selector-container">
-                    <button className="language-selector-btn">
+                    <button className="language-selector-btn" 
+                    onClick={() => setIsOpen(!isOpen)}
+                    onKeyDown={(e) => { 
+                        if (e.key === 'Escape') setIsOpen(false)
+                        }}>
                         <img className="language-selector-icon" src={languageIcon} alt="language selector icon"></img>
                         <span className="sr-only">select language</span>
                     </button>
-                    <ul className="language-flag-container">
+                    {isOpen && (<ul className="language-flag-container">
                         <li>
-                            <img role="button" tabIndex={0} className="english-language flag" src={englishFlag} onClick={() => i18n.changeLanguage('en')} alt="select english language"></img>
+                            <button onClick={() => { i18n.changeLanguage('en'); setIsOpen(false) }}>
+                                <img className="english-language flag" src={englishFlag} alt="select english language"></img>
+                            </button>
                         </li>
                         <li>
-                            <img role="button" tabIndex={0} className="german-language flag" src={germanFlag} onClick={() => i18n.changeLanguage('de')} alt="select german language"></img>
+                            <button onClick={() => { i18n.changeLanguage('en'); setIsOpen(false) }}>
+                                <img className="german-language flag" src={germanFlag} alt="select german language"></img>
+                            </button>
                         </li>
                         <li>
-                            <img role="button" tabIndex={0} className="indonesian-language flag" src={indonesiaFlag} onClick={() => i18n.changeLanguage('id')} alt="select indonesian language"></img>
+                            <button onClick={() => { i18n.changeLanguage('en'); setIsOpen(false) }}>
+                                <img className="indonesian-language flag" src={indonesiaFlag} alt="select indonesian language"></img>
+                            </button>
                         </li>
                     </ul>
+                )}
                 </div>
             </div>
         </header>
